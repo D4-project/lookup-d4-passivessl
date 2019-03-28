3508353f456a5500cf54124219673276ce911ecb
Jean-Louis Huynen <huynenjl@gmail.com>
2019-03-28 15:26:36 +0100

    Quick n Dirty implem

--- main.go
+++ main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"crypto/x509"
 	"database/sql"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	_ "github.com/lib/pq"
 )
 
+type certMapElm struct {
+	CertHash string
+	*x509.Certificate
+}
+
+type sessionRecord struct {
+	ServerIP     string
+	ServerPort   string
+	ClientIP     string
+	ClientPort   string
+	TLSH         string
+	Timestamp    time.Time
+	JA3          string
+	JA3Digest    string
+	JA3S         string
+	JA3SDigest   string
+	Certificates []certMapElm
+}
+
 type sessions struct {
 	Sessions []dbSession
+}
+
+type usessions struct {
+	Sessions []sessionRecord
 }
 
 type dbSession struct {
@@ -21,46 +49,113 @@ type dbSession struct {
 var db *sql.DB
 
 func main() {
+	// connect to db
+	connStr := "user=postgres password=postgres dbname=passivessl"
+	err := errors.New("")
+	db, err = sql.Open("postgres", connStr)
+	defer db.Close()
+	if err != nil {
+		panic(err)
+	}
 
 	http.HandleFunc("/index", indexHandler)
+	http.HandleFunc("/ja3/", ja3Handler)
+	http.HandleFunc("/ja3s/", ja3sHandler)
 
-	err := http.ListenAndServe("localhost:8000", nil)
+	err = http.ListenAndServe("localhost:8000", nil)
 	if err != nil {
-		log.Fatal("HTTP Sever: ", err)
+		log.Fatal("HTTP Server: ", err)
 	}
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	s := sessions{}
+func parseParam(req *http.Request, prefix string) (string, error) {
+	param := strings.TrimPrefix(req.URL.Path, prefix)
+	return param, nil
+}
 
-	err := querySessions(&s)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	s := usessions{}
+
+	err := queryDB(&s, `
+        SELECT
+            id,
+            data
+        FROM sessions 
+        ORDER BY id ASC`)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, s.Sessions[0].data)
-	// for _, s := range s.Sessions {
-	// 	fmt.Fprintf(w, s.data)
-	// }
+	out, err := json.Marshal(s)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	fmt.Fprintf(w, string(out))
 }
 
-func querySessions(s *sessions) error {
-	// connect to db // PARKING HERE
-	connStr := "user=postgres password=postgres dbname=passivessl"
-	db, err := sql.Open("postgres", connStr)
-	defer db.Close()
+func ja3Handler(w http.ResponseWriter, r *http.Request) {
+	param, err := parseParam(r, "/ja3/")
 	if err != nil {
-		panic(err)
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
 	}
+	s := usessions{}
 
-	rows, err := db.Query(`
+	err = queryDB(&s, `
         SELECT
             id,
             data
-        FROM sessions 
-        ORDER BY id ASC`)
+		FROM sessions
+		WHERE data ->> 'JA3Digest' = '`+param+`'
+        ORDER BY id ASC	`)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	out, err := json.Marshal(s)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	fmt.Fprintf(w, string(out))
+}
+
+func ja3sHandler(w http.ResponseWriter, r *http.Request) {
+	param, err := parseParam(r, "/ja3s/")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+	s := usessions{}
+
+	err = queryDB(&s, `
+        SELECT
+            id,
+            data
+		FROM sessions
+		WHERE data ->> 'JA3SDigest' = '`+param+`'
+        ORDER BY id ASC	`)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	out, err := json.Marshal(s)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	fmt.Fprintf(w, string(out))
+}
+
+func queryDB(s *usessions, q string) error {
+	rows, err := db.Query(q)
 	if err != nil {
 		return err
 	}
@@ -73,7 +168,9 @@ func querySessions(s *sessions) error {
 		if err != nil {
 			return err
 		}
-		s.Sessions = append(s.Sessions, sl)
+		tmp := sessionRecord{}
+		json.Unmarshal([]byte(sl.data), &tmp)
+		s.Sessions = append(s.Sessions, tmp)
 	}
 	err = rows.Err()
 	if err != nil {
